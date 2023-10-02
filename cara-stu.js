5e7d5aa196acfcad89fb1e1085da5f2099aeca1b
zidarmaozi <91011478+zidarmaozi@users.noreply.github.com>
2023-10-02 13:24:27 +0800

    Add files via upload

---
+++ cara-stu.js
@@ -0,0 +1,18 @@
+<p id="cara-link"></p>
+<script>
+document.getElementById("cara-link").innerHTML = `
+<p>Berikut ini adalah cara melewati link Subs4unlock di adirga.com</p>
+<ol class="step">
+  <li>pertama buka link yang ingin kalian tuju (pastikan kalian membukanya di chorome atau browser lain)</li>
+  <li>Jika sudah muncul seperti ini tunggu sampai proses generating link selesai,kira kira sekitar 3detik. 
+    <div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="589" data-original-width="1080" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEinonRZ3utCPCMKB5JkuuhXyMnCPjy_CROgwkjoiY6cuFqMQuCG-ywhmYKR8MgapQUdLWVPClFR7nYVMymrFi3xQhDXPqk9IM5wsMoP8OvMVxsFLwNH4r9qKvCxqr0k3kpog0j-zXW6kEssXTvJ-p-svMpWPSKv3RACV9UuQu95QLPM43lLO7f4vbhOG8lj/s1600/IMG_20231002_124622.jpg"/></div></li>
+    
+   <li>Kalau sudah muncul seperti gambar dibawah klik tombol i'am not a robot.
+     <div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="635" data-original-width="1080" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjkkwwT2ax0YTiXaAfUQbdGxBwrTE_VKuNoQ0kFHJ8zniLBRaHeBCvuLFWLlRBF350ww4k6fTqEriPvldXxNtyHB17jFJepbq3I03SEKGyl491G3Hy6CH4wwAcYEwXHYpBm63WPJ4xF9__bhMoNsgfw3JjVIzAwwHmEYk_FoSxY35HIV5KOWP8nGNFwvE5I/s1600/IMG_20231002_124604.jpg"/></div></li>
+  <li>Intinya kalian ikuti perintahnya aja cuy<div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="1503" data-original-width="1080" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjEXzY2Ih1NoBw1jAfvo23qui93Sm4Il8o9popbOlV9e2Sx-bkDOJkITp84UV7NUeEBnBt9euZwjWGJkFqOYSQxA6BBsRkW9kdJncafgUxqDBIe5Hs8CaUFuTctEiL8PzQYnkCQw-9itQsYo7ffN-sKep7W4Bk0ZP4UPBb6pviQRhDyAbs-db4UssFTMzko/s1600/IMG_20231002_125735.jpg"/></div></li>
+    <li><div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="1351" data-original-width="1079" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg9BBcEQWNFH_xmiVc1eydrRcsPaa0No8-IgNqSuR8PRncs9rIFYmXhAxpHJHZ8wCSRSjRRxQG8jg-Cuzuo3ozr8QP1wtpqJQswjdKw9j-TsW6pSHsxEDhbRLJ02lOsFQP1CSWbk-r2697ZCUWImQ3XZMTlObr2NOGef3am3SM57TDV4C5MN8-HiWspkNnx/s1600/Screenshot_2023-10-02-12-44-48-913_com.android.chrome.png"/></div></li>
+  <li><div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="1222" data-original-width="1080" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhAo9Bkw3okV6CiAOqvJE_7Rst6RrxICrsO3WZCqqriC85_aogKHoPp2Nffd__gMbvHjRsRLKwtw9yvcwk0-9ORp7p5wBIECJll0KtSBzIAA5KwXAQa7gUXVyDz-IQOHePYAlXrLM3T89tGoC0GkjsER9FAxtQv-d3RWXwFzlpUAX-dGRyugKeMBt7HjFuq/s1600/IMG_20231002_124546.jpg"/></div></li>
+    <li><div class="separator" style="clear: both;"><img alt="" border="0" data-original-height="1190" data-original-width="1079" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjhVoC1S6O25vSHg6y6IVlsYUbBcCevLs254t694YnzZZmxBDLcLC2g-3TkdAAnOJ6pC0fhMr31UrpteiQ_kFL43t2LU1eqbblVmo_a3WDNAQyylWUHPCO2wqSfVYCfr2_SfMDrUCygXnWqdM5cMnPpnyY8UafTo8GyU75ekyLYvjwGccBvhS7BffqdIdtY/s1600/Screenshot_2023-10-02-12-45-25-054_com.android.chrome.png"/></div></li>
+  <li>Selesai</li></ol>
+`;
+</script>
